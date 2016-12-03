e0392585d65501d14e8ce49d60b99f993f434a5d
VitorGCarrilho <vitorg.carrilho@gmail.com>
2016-12-03 14:30:51 -0200

    Inicio do projeto

---
+++ ti-antenado/src/main/java/br/com/tiantenado/TiAntenadoApplication.java
@@ -0,0 +1,12 @@
+package br.com.tiantenado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiAntenadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiAntenadoApplication.class, args);
+	}
+}
