c92ff5e716bcdbf023264c06feb97bb2991c8e84
VitorGCarrilho <vitorg.carrilho@gmail.com>
2016-12-03 16:39:44 -0200

    Conexao com o banco de dados

--- ti-antenado/src/main/java/br/com/tiantenado/controller/TiAntenadoController.java
+++ ti-antenado/src/main/java/br/com/tiantenado/controller/TiAntenadoController.java
@@ -1,13 +1,18 @@
 package br.com.tiantenado.controller;
 
+import java.sql.Connection;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.tiantenado.config.DataBaseConfig;
 
 @Controller
 public class TiAntenadoController {
 
 	@RequestMapping("/home")
 	public String home(){
+			
 		return "index";
 	}
 	
