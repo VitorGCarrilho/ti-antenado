db6f6075d42b9df317a0c89b680404f4acfbbd1d
marciomarinho <marciomarinho@gmail.com>
2016-12-06 13:54:35 +1100

    added configuration on application.properties, moved your sql to resources/db/migrations/V1__filename, added stuff on pom.xml and removed ServletInitializer and DataBaseConfig.

--- ti-antenado/src/main/java/br/com/tiantenado/ServletInitializer.java
+++ ti-antenado/src/main/java/br/com/tiantenado/ServletInitializer.java
@@ -3,11 +3,11 @@ package br.com.tiantenado;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class ServletInitializer { //extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TiAntenadoApplication.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(TiAntenadoApplication.class);
+//	}
 
 }
--- ti-antenado/src/main/java/br/com/tiantenado/config/DataBaseConfig.java
+++ ti-antenado/src/main/java/br/com/tiantenado/config/DataBaseConfig.java
@@ -6,19 +6,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseConfig {
-	public static Connection getConnection(){
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			return DriverManager.getConnection("jdbc:mysql://localhost:3306/TI_ANTENADO?autoReconnect=true&useSSL=false", "root", "");
-		} catch (ClassNotFoundException e) {
-			System.out.println("ClassNotFoundException");
-			e.printStackTrace();
-			return null;
-		} catch (SQLException e) {
-			System.out.println("SQLException");
-			e.printStackTrace();
-			return null;
-		}
-		
-	}
+//	public static Connection getConnection(){
+//		try {
+//			Class.forName("com.mysql.jdbc.Driver");
+//			return DriverManager.getConnection("jdbc:mysql://localhost:3306/TI_ANTENADO?autoReconnect=true&useSSL=false", "root", "");
+//		} catch (ClassNotFoundException e) {
+//			System.out.println("ClassNotFoundException");
+//			e.printStackTrace();
+//			return null;
+//		} catch (SQLException e) {
+//			System.out.println("SQLException");
+//			e.printStackTrace();
+//			return null;
+//		}
+//
+//	}
 }
