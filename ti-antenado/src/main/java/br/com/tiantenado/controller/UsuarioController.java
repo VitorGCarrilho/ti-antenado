8abc28bf94d1b47a1f092287f12f598106e38202
VitorGCarrilho <vitorg.carrilho@gmail.com>
2017-01-28 00:17:48 -0200

    Serviço rest

---
+++ ti-antenado/src/main/java/br/com/tiantenado/controller/UsuarioController.java
@@ -0,0 +1,31 @@
+package br.com.tiantenado.controller;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.tiantenado.model.Usuario;
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController {
+	
+	@RequestMapping(value="/cadastro", method= RequestMethod.POST)
+	public ModelAndView inserirUsuario(@ModelAttribute("usuario") Usuario usuario){	
+		ModelAndView mv = new ModelAndView("InserirUsuario");
+		mv.addObject("usuario",usuario);
+		return mv;
+	}
+	
+	@RequestMapping("/cadastro")
+	public ModelAndView inserirUsuario(){		
+		Usuario usuario = new Usuario();
+		ModelAndView mv = new ModelAndView("InserirUsuario");
+		mv.addObject("usuario",usuario);
+		return mv;
+	}
+}
