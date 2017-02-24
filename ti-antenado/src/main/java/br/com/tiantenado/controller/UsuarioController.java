7f9cf7d1f36c57dc92beca178c9135b045aeedc8
Vitor Carrilho <vitorg.carrilho@gmail.com>
2017-02-24 20:00:08 -0300

    Alteracao no form de cadastro de aluno e professor na index

--- ti-antenado/src/main/java/br/com/tiantenado/controller/UsuarioController.java
+++ ti-antenado/src/main/java/br/com/tiantenado/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import static org.springframework.test.web.client.match.MockRestRequestMatchers.
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,7 +15,7 @@ import br.com.tiantenado.model.Usuario;
 @RequestMapping("/usuarios")
 public class UsuarioController {
 	
-	@RequestMapping(value="/cadastro", method= RequestMethod.POST)
+	@PostMapping(value="/cadastro")
 	public ModelAndView inserirUsuario(@ModelAttribute("usuario") Usuario usuario){	
 		ModelAndView mv = new ModelAndView("Usuario/InserirUsuario");
 		mv.addObject("usuario",usuario);
