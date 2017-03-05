7088d7838814dee68d5ce83edc480bf5fdd151dd
VitorGCarrilho <vitorg.carrilho@gmail.com>
2017-03-05 20:12:00 -0300

    HTTP Basic

--- ti-antenado/src/main/java/br/com/tiantenado/config/WebSecurityConfig.java
+++ ti-antenado/src/main/java/br/com/tiantenado/config/WebSecurityConfig.java
@@ -48,6 +48,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 					.and()
 			.logout()
 					.logoutSuccessUrl("/login?logout")
-					.permitAll();
+					.permitAll()
+					.and()
+			.httpBasic();
 	}
 }
--- ti-antenado/src/main/java/br/com/tiantenado/resources/CursosResources.java
+++ ti-antenado/src/main/java/br/com/tiantenado/resources/CursosResources.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,7 @@ public class CursosResources {
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@GetMapping()
 	public ResponseEntity<?> getById(@PathVariable(name="id") long id){
 		Curso curso = cursoService.findById(id);	
 		CacheControl cacheControl = CacheControl.maxAge(1, TimeUnit.MINUTES);
